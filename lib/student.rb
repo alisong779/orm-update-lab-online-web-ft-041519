f6677d1814baf338eda312bf62ae821c37c1e184
Alison Golden <alisong779@yahoo.com>
2019-05-22 15:57:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,13 @@
 require_relative "../config/environment.rb"
 
 class Student
+  attr_accessor :name, :grade, :id
+  
+  def initialize(name, grade, id=nil)
+    @name = name 
+    @grade = grade
+    @id = id 
+  end 
 
 
 
